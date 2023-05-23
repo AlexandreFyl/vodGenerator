ccb21ccaedb58c8a0685f5f23c3535e918bf5594
AlexandreFyl <afayolle43@gmail.com>
2023-05-23 14:13:52 +0200

    add a get method for loading screen picture using official riot datadragon and not the community edition. Rework of pfp method with DDragon too. We keep version method for the moment but deprecated.

--- scripts/getAssets.py
+++ scripts/getAssets.py
@@ -1,6 +1,9 @@
 import requests
 import os
 
+# CONSTANTS
+TEMP_FOLDER_PATH = "../assets/temp"
+
 
 def getLastVersionAvailable():
     response = requests.get(
@@ -8,13 +11,14 @@ def getLastVersionAvailable():
 
     data = response.json()
 
+    # Return last version number
+    # So we ensure to get updated data at any moment
     return data[0]
 
 
-def getChampProfilePicture(champName, patch):
+def getChampProfilePicture(champName):
     # Create a URL string.
-    url = "https://cdn.communitydragon.org/" + \
-        patch + "/champion/" + champName + "/square"
+    url = "http://ddragon.leagueoflegends.com/cdn/13.10.1/img/champion/" + champName + ".png"
 
     # Prepare the request.
     prepared_request = requests.Request("GET", url).prepare()
@@ -25,17 +29,29 @@ def getChampProfilePicture(champName, patch):
     # Get the response.
     data = response.content
 
-    # Specify the temp assets folder path.
-    temp_folder_path = "../assets/temp"
-
-    # Create the folder if it doesn't exist.
-    if not os.path.exists(temp_folder_path):
-        os.makedirs(temp_folder_path)
-
     # Specify the file path.
-    file_path = os.path.join(temp_folder_path, "champion-profile-icon.png")
+    file_path = os.path.join(TEMP_FOLDER_PATH, "champion-profile-picture.png")
 
     # Save the response to the file.
+    with open(file_path, "wb") as f:
+        f.write(data)
+
+# please refers to first method comments
+
+
+def getChampLoadingScreenPicture(champName):
+    url = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + \
+        champName + "_0.jpg"
+
+    prepared_request = requests.Request("GET", url).prepare()
+
+    response = requests.Session().send(prepared_request)
+
+    data = response.content
+
+    file_path = os.path.join(
+        TEMP_FOLDER_PATH, "champion-loading-screen-picture.jpg")
+
     with open(file_path, "wb") as f:
         f.write(data)
 
@@ -45,4 +61,5 @@ def getChampProfilePicture(champName, patch):
 
 # TESTS
 PATCH = getLastVersionAvailable()
-getChampProfilePicture("yuumi", PATCH)
+getChampProfilePicture("Yuumi")
+getChampLoadingScreenPicture("Yuumi")
